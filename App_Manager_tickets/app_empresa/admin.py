f8ca8aa505d6ea45425907753338311dcb34f416
keev21 <kevinsan1835@gmail.com>
2025-05-24 15:25:54 -0500

    Eliminacion de carpetas innecesarias

--- App_Manager_tickets/app_empresa/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Empresa)
-admin.site.register(Sucursal)
-admin.site.register(Departamento)
-# Register your models here.
--- App_Manager_tickets/app_usuario/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Rol)
-admin.site.register(Usuario)
-
-# Register your models here.
