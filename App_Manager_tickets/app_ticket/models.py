9266acc902d84013eb59a291f6d879d8a11afb8d
Cristian Defaz <cristiandefaz191@gmail.com>
2025-05-30 16:14:52 -0500

    borrado duplicado solucion

--- App_Manager_tickets/app_ticket/models.py
+++ App_Manager_tickets/app_ticket/models.py
@@ -116,12 +116,3 @@ class EvaluacionTecnico(models.Model):
 
     def __str__(self):
         return f"Evaluación del Ticket #{self.ticket.id} - {self.get_calificacion_display(),}"
-    
-class SolucionTicket(models.Model):
-    ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE, related_name='imagenes')
-    ruta_imagen=models.ImageField(upload_to='solucion_tickets', blank=True, null=True)
-    comentario = models.TextField(null=True, blank=True)
-    fecha_subida = models.DateTimeField(auto_now_add=True)
-
-    def _str_(self):
-        return f"Solucion ID {self.ticket.id}"
