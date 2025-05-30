217a0840c89f8f1396b26ce18878d64a7a8f64cc
keev21 <kevinsan1835@gmail.com>
2025-05-30 18:50:49 -0500

    Diseño de inicio de sesion

--- App_Manager_tickets/App_Manager_tickets/urls.py
+++ App_Manager_tickets/App_Manager_tickets/urls.py
@@ -21,6 +21,7 @@ from app_ticket import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     #nuevo
     path('empresa/nuevo/',views.empresa_nueva, name='empresa_nuevo'),
     path('categoria/nuevo/',views.categoria_nueva, name='categoria_nuevo'),
@@ -69,5 +70,9 @@ urlpatterns = [
     path('perfil/', views.perfil, name='perfil'),
     #completar registro
     path('usuario/completar_registro/', views.completar_registro, name='completar_registro'),
+    path('dashboard/', views.ticket_editar, name='dashboard'),
 
 ]
+
+
+    
